c8e4268a144bbc89652ef738f88d4034f3d70b2d
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-04-27 15:50:14 -0900

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""this funtion Augment the following code with the correct duck-typed annotations"""
+"""Augmentation of the following code with the correct duck-typed annotations"""
 from typing import Any, Union, Sequence
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """input the elements of the input are not know"""
+    """types the elements of the input are not know"""
     if lst:
+    
         return lst[0]
+    
     else:
         return None
