deb3f01d0e27cb8721de39f3f1c0e7a50c9ee06a
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-04-27 15:20:03 -0900

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+This module supplies the annonated function `add`.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds float a and b and returns the result"""
+    return a + b
