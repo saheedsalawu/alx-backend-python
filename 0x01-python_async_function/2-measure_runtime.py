e50a79360c371b5131099601d2cbfa40d21930ef
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-05-06 12:31:34 -0900

    Update 2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-"""execute multiple coroutines at the same time with async"""
+"""From the previous file, import wait_n into 2-measure_runtime.py.
+Create a measure_time function with integers n and max_delay as
+arguments that measures the total execution time for wait_n(n, max_delay),
+and returns total_time / n. Your function should return a float.
+Use the time module to measure an approximate elapsed time.
+"""
 
+
+import time
 import asyncio
-from typing import List
-
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    2 int arguments (in this order): n and max_delay
-    async routine called wait_n
-    """
+def measure_time(n: int, max_delay: int) -> float:
+    """Returns total execution time"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
 
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    total_time = end_time - start_time
+    return (total_time/n)
