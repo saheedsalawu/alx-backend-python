1f9a57c77eb253b4347c3732f24b4d8d842a4bab
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-05-06 12:29:15 -0900

    Update 2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-""" To approximate elapsed Measure the runtime"""
-from time import perf_counter
+"""execute multiple coroutines at the same time with async"""
+
 import asyncio
-wait_n = __import__('1-concurrent_coroutines').wait_n
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    
-    start = perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    elapsed = perf_counter() - start
-    return elapsed / n
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    2 int arguments (in this order): n and max_delay
+    async routine called wait_n
+    """
+
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
